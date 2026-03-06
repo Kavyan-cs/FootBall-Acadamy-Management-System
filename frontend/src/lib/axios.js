27de7f7833174518276564a336b93652e672a195
Kavyan-cs <kavyanshinde0@gmail.com>
2026-03-06 18:49:41 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://localhost:3000"
-   baseURL: "https://football-acadamy-management-system.onrender.com",
+   baseURL: "https://football-acadamy-management-system.onrender.com/api/players",
   // https://football-acadamy-management-system.onrender.com/api/players
 });
 
